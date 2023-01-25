90ad163c17e639be40736ddbae2923b1aa8eea36
kumarSonal308 <121526717+kumarSonal308@users.noreply.github.com>
2023-01-25 18:24:02 +0530

    gg

---
+++ techystar/src/components/Footer.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Footer = () => {
+  return <footer>
+     <div>
+        <h1>TechStar</h1>
+        <p>@all right reserved</p>
+     </div>
+
+     <div>
+        <h5>Follow US</h5>
+        <div >
+            <a href='https://www.youtube.com/@infocreator3098/featured' target={"blank"}>
+                Youtube</a>
+            <a href='https://www.instagram.com/sonal_kumar_308/?hl=en' target={"blank"}>
+                Instagram</a>
+            
+        </div>
+     </div>
+  </footer>
+
+}
+
+export default Footer
